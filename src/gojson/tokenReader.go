0b7a548986ba921ddf5bdcdceb8123a347208217
gaufung <gaufung@outlook.com>
2018-03-25 17:41:46 +0800

    fix return

--- src/gojson/tokenReader.go
+++ src/gojson/tokenReader.go
@@ -4,6 +4,8 @@ import (
 	"math"
 	"strings"
 	"io"
+	"errors"
+	"strconv"
 )
 
 const (
@@ -17,7 +19,7 @@ type TokenReader struct {
 	reader *CharReader
 }
 
-func newTokenReaderFromString(s string) *TokenReader {
+func NewTokenReaderFromString(s string) *TokenReader {
 	return newTokenReader(strings.NewReader(s))
 }
 
@@ -29,17 +31,21 @@ func (t *TokenReader) position() int {
 	return t.reader.pos
 }
 
+func (t *TokenReader) errors(info string) error{
+	return errors.New(info+" at " + strconv.Itoa(t.position()))
+}
+
 // white space to ignore
 func (t *TokenReader) isWhiteSpace(ch rune) bool {
 	return ch == '\n' || ch == '\t' || ch == ' ' || ch == '\r'
 }
 
 // read next token
-func (t *TokenReader) readNextToken() Token {
+func (t *TokenReader) readNextToken() (Token,error) {
 	ch := '?'
 	for {
 		if !t.reader.hasMore() {
-			return END_DOCUMENT
+			return END_DOCUMENT, nil
 		}
 		ch = t.reader.peek() //peek it
 		if !t.isWhiteSpace(ch) {
@@ -50,45 +56,46 @@ func (t *TokenReader) readNextToken() Token {
 	switch ch {
 	case '{':
 		t.reader.next() //skip
-		return START_OBJECT
+		return START_OBJECT, nil
 	case '}':
 		t.reader.next() //skip
-		return END_OBJECT
+		return END_OBJECT, nil
 	case '[':
 		t.reader.next() //skip
-		return START_ARRAY
+		return START_ARRAY, nil
 	case ']':
 		t.reader.next() //skip
-		return END_ARRAY
+		return END_ARRAY, nil
 	case ':':
 		t.reader.next() //skip
-		return COLON_SEPERATOR
+		return COLON_SEPERATOR, nil
 	case ',':
 		t.reader.next() //skip
-		return COMA_SEPERATOR
+		return COMA_SEPERATOR, nil
 	case '"':
-		return STRING
+		return STRING, nil
 	case 'n':
-		return NULL
+		return NULL, nil
 	case 't':
-		return BOOLEAN
+		return BOOLEAN, nil
 	case 'f':
-		return BOOLEAN
+		return BOOLEAN, nil
 	case '-':
-		return NUMBER
+		return NUMBER, nil
 	}
 	if ch >= '0' && ch <= '9' {
-		return NUMBER
+		return NUMBER, nil
 	}
-	panic(NewJsonParserError("Parse error when try to guess next token.", t.reader.pos))
+	//panic(NewJsonParserError("Parse error when try to guess next token.", t.reader.pos))
+	return -1, t.errors("Unexpected token ")
 }
 
 // read string
-func (t *TokenReader) readString() string {
+func (t *TokenReader) readString() (string,error) {
 	result := make([]rune, 0)
 	ch := t.reader.next()
 	if ch != '"' {
-		panic(NewJsonParserError("Expected \" but actual is: ", t.reader.pos))
+		return "", t.errors("Unexpected string")
 	}
 	for {
 		ch = t.reader.next()
@@ -122,26 +129,27 @@ func (t *TokenReader) readString() string {
 					} else if uch >= 'A' && uch <= 'F' {
 						u = (u << 4) + (int(uch) - int('A')) + 10
 					} else {
-						panic(NewJsonParserError("Unexpected char", t.reader.pos))
+						//panic(NewJsonParserError("Unexpected char", t.reader.pos))
+						return "", t.errors("Read string: unexpected char ")
 					}
 				}
 				result = append(result, rune(u))
 			default:
-				panic(NewJsonParserError("Unexpected char", t.reader.pos))
+				return "", t.errors("Read string: unexpected char")
 			}
 		} else if ch == '"' {
 			break
 		} else if ch == '\r' || ch == '\n' {
-			panic(NewJsonParserError("Unexpected char", t.reader.pos))
+			return "", t.errors("Read string: unexpected char")
 		} else {
 			result = append(result, ch)
 		}
 	}
-	return string(result)
+	return string(result), nil
 }
 
 // read boolean
-func (t *TokenReader) readBoolean() bool {
+func (t *TokenReader) readBoolean() (bool,error) {
 	ch := t.reader.next()
 	expect := ""
 	if ch == 't' {
@@ -149,29 +157,31 @@ func (t *TokenReader) readBoolean() bool {
 	} else if ch == 'f' {
 		expect = "alse"
 	} else {
-		panic(NewJsonParserError("Unexpected char", t.reader.pos))
+		//panic(NewJsonParserError("Unexpected char", t.reader.pos))
+		return false, t.errors("Read boolean: unexpected char ")
 	}
 	for _, c := range []rune(expect) {
 		theChar := t.reader.next()
 		if theChar != c {
-			panic(NewJsonParserError("Unexpected char", t.reader.pos))
+			return false, t.errors("Read boolean: unexpected char ")
 		}
 	}
-	return ch == 't'
+	return ch == 't', nil
 }
 
-func (t *TokenReader) readNull() {
+func (t *TokenReader) readNull() error {
 	expect := "null"
 	for _, c := range []rune(expect) {
 		theChar := t.reader.next()
 		if theChar != c {
-			panic(NewJsonParserError("Unexpected char", t.reader.pos))
+			return t.errors("Read null: unexpected char")
 		}
 	}
+	return nil
 }
 
 // read number
-func (t *TokenReader) readNumber() float64 {
+func (t *TokenReader) readNumber() (float64, error) {
 	intPart, fraPart, expPart := make([]rune, 0), make([]rune, 0), make([]rune, 0)
 	hasFraPart, hasExpPart := false, false
 	ch := t.reader.peek()
@@ -193,7 +203,7 @@ func (t *TokenReader) readNumber() float64 {
 				intPart = append(intPart, t.reader.next())
 			} else if ch == '.' {
 				if len(intPart) == 0 {
-					panic(NewJsonParserError("Unexpected char", t.reader.pos))
+					return 0.0, t.errors("Read float64: unexpect char ")
 				}
 				t.reader.next()
 				hasFraPart = true
@@ -209,7 +219,7 @@ func (t *TokenReader) readNumber() float64 {
 				status = READ_NUMBER_EXP_PART
 			} else {
 				if len(intPart) == 0 {
-					panic(NewJsonParserError("Unexpected char", t.reader.pos))
+					return 0.0, t.errors("Read float64: unexpect char ")
 				}
 				status = READ_NUMBER_END
 			}
@@ -228,7 +238,8 @@ func (t *TokenReader) readNumber() float64 {
 				status = READ_NUMBER_EXP_PART
 			} else {
 				if len(fraPart) == 0 {
-					panic(NewJsonParserError("Unexpected char", t.reader.pos))
+					//panic(NewJsonParserError("Unexpected char", t.reader.pos))
+					return 0.0, t.errors("Read float64, unexpected char ")
 				}
 				status = READ_NUMBER_END
 			}
@@ -238,52 +249,81 @@ func (t *TokenReader) readNumber() float64 {
 				expPart = append(expPart, t.reader.next())
 			} else {
 				if len(expPart) == 0 {
-					panic(NewJsonParserError("Unexpected char", t.reader.pos))
+					return 0.0, t.errors("Read float64, unexpected char ")
 				}
 				status = READ_NUMBER_END
 			}
 			continue
 		case READ_NUMBER_END:
-			readed := t.reader.pos
 			if len(intPart) == 0 {
-				panic(NewJsonParserError("Unexpected char", t.reader.pos))
+				return 0.0, t.errors("Read float64, unexpected char ")
 			}
 			lint := int64(0)
 			if minusSign {
-				lint = -1 * string2long(intPart, readed)
+				//lint = -1 * string2long(intPart, readed)
+				if val, err:=string2long(intPart, t.position()); err==nil{
+					lint = -1 * val
+				}else{
+					return 0.0, err
+				}
 			} else {
-				lint = string2long(intPart, readed)
+				//lint = string2long(intPart, readed)
+				if val, err := string2long(intPart, t.position()); err==nil{
+					lint = val
+				}else{
+					return 0.0, err
+				}
 			}
 			if hasExpPart && len(expPart) == 0 {
-				return float64(lint)
+				return float64(lint), nil
 			}
 			if hasFraPart && len(fraPart) == 0 {
-				panic(NewJsonParserError("Unexpected char", t.reader.pos))
+				return 0.0, t.errors("Read float64, unexpected char ")
 			}
 			dFraPart := float64(0.0)
 			if hasFraPart {
 				if minusSign {
-					dFraPart = -float64(1.0) * string2Fraction(fraPart, t.reader.pos)
+					//dFraPart = -float64(1.0) * string2Fraction(fraPart, t.reader.pos)
+					if val, err := string2Fraction(fraPart, t.position()); err == nil{
+						dFraPart = -float64(1.0) * val
+					}else{
+						return 0.0, err
+					}
 				} else {
-					dFraPart = string2Fraction(fraPart, t.reader.pos)
+					//dFraPart = string2Fraction(fraPart, t.reader.pos)
+					if val, err := string2Fraction(fraPart, t.position()); err==nil{
+						dFraPart = val
+					}else{
+						return 0.0, err
+					}
 				}
 			}
 			number := float64(0.0)
 			if hasExpPart {
 				index := int64(0.0)
 				if expMinusSign {
-					index = -1.0 * string2long(expPart, readed)
+					//index = -1.0 * string2long(expPart, readed)
+					if val, err := string2long(expPart, t.position()); err==nil{
+						index = -1.0 * val
+					}else{
+						return 0.0, err
+					}
 				} else {
-					index = string2long(expPart, readed)
+					//index = string2long(expPart, readed)
+					if val, err := string2long(expPart, t.position()); err!=nil{
+						index = val
+					}else{
+						return 0.0, err
+					}
 				}
 				number = (float64(lint) + dFraPart) * math.Pow(10.0, float64(index))
 			} else {
 				number = float64(lint) + dFraPart
 			}
 			if number > maxSafeFloat64 {
-				panic(NewJsonParserError("Exceeded maximum value", t.reader.pos))
+				return 0.0, t.errors("Read float64, unexpected char ")
 			}
-			return number
+			return number, nil
 		}
 	}
 }
@@ -304,25 +344,25 @@ func (t *TokenReader) IsEmpty() bool {
 
 var maxSafeInt64 = int64(9007199254740991)
 
-func string2long(str []rune, readed int) int64 {
+func string2long(str []rune, readed int) (int64, error) {
 	if len(str) > 16 {
-		panic(NewJsonParserError("Number string is too long", readed))
+		return 0.0, errors.New("Read long: too many digits at "+strconv.Itoa(readed))
 	}
 	result := int64(0)
 	for _, digit := range str {
 		result = result*10 + int64(digit-'0')
 		if result > maxSafeInt64 {
-			panic(NewJsonParserError("Exceeded maximum value", readed))
+			return 0.0, errors.New("Read long: too big at "+ strconv.Itoa(readed))
 		}
 	}
-	return result
+	return result, nil
 }
 
 var maxSafeFloat64 = 1.7976931348623157e+308
 
-func string2Fraction(str []rune, readed int) float64 {
+func string2Fraction(str []rune, readed int) (float64, error) {
 	if len(str) > 16 {
-		panic(NewJsonParserError("Number string is too long", readed))
+		return 0.0, errors.New("Read fraction: too many digits at " +strconv.Itoa(readed))
 	}
 	result := float64(0.0)
 	for idx, digit := range str {
@@ -333,5 +373,5 @@ func string2Fraction(str []rune, readed int) float64 {
 			result = result + float64(n)/math.Pow(10, float64(idx+1))
 		}
 	}
-	return result
+	return result, nil
 }
